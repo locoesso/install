9aa27f63fe4239b7c5b52bb80b1aba97c0cdcb37
Esa Hanikka <esa.hanikka@gmail.com>
2017-10-21 10:13:06 +0300

    Done something

--- ubuntu/yle-dl.sh
+++ ubuntu/yle-dl.sh
@@ -2,8 +2,6 @@
 
 # https://github.com/aajanki/yle-dl
 
-sudo apt install rtmpdump wget python python-setuptools python-pip python-crypto python-requests python-lxml php-cli php-curl php-mcrypt php-xml php-bcmath
-sudo phpenmod mcrypt
-sudo apt install python-pip
+sudo apt install rtmpdump wget python python-setuptools python-pip python-crypto python-requests python-lxml php-cli php-curl php-mcrypt php-xml php-bcmath mcrypt
 sudo pip install yle-dl
 
