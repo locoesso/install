815f011529c4c3a43e0baa25eb792e06a8a120d9
locoesso <esa.hanikka@gmail.com>
2018-01-08 14:46:11 +0200

    s

--- arch/git-setup.sh
+++ arch/git-setup.sh
@@ -2,6 +2,6 @@
 
 # Set the cache to timeout after 1 hour (setting is in seconds)
 git config --global credential.helper 'cache --timeout=3600'
-git config --global user.name "locoesso" 
-git config --global user.email esa.hanikka@gmail.com
+git config --global user.name "xxx" 
+git config --global user.email xxx@xxx.com
 git config --global core.editor nano
