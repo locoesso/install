82003d655f2d16653f6b725d7398dfca724af588
Esa Hanikka <esa.hanikka@gmail.com>
2018-01-14 19:36:29 +0200

    Solus aliases added

---
+++ solus/git/pop-gtk-theme.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo eopkg it libsass sassc inkscape optipng make
+
+mkdir -p $HOME/.git-install
+cd $HOME/.git-install
+git clone https://github.com/pop-os/gtk-theme
+
+cd $HOME/.git-install/gtk-theme
+sudo make uninstall
+sudo rm -rf /usr/share/themes/{Pop,Pop-Eta,Pop-Nokto,Pop-Nokto-Eta}
+rm -rf ~/.local/share/themes/{Pop,Pop-Eta,Pop-Nokto,Pop-Nokto-Eta}
+rm -rf ~/.themes/{Pop,Pop-Eta,Pop-Nokto,Pop-Nokto-Eta}
+
+make
+
+sudo make install
+
---
+++ solus/minecraft.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo eopkg it openjdk-8
+sudo mkdir -p /opt/minecraft
+sudo wget http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar -O /opt/minecraft/Minecraft.jar
+sudo cp minecraft.desktop /usr/share/applications/
+sudo update-desktop-database
+
