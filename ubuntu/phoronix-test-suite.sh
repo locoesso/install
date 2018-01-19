4fb724db7b70b138157117dd7ec32fcd630e036f
Esa Hanikka <esa.hanikka@gmail.com>
2018-01-19 15:32:04 +0200

    1111

--- ubuntu/phoronix-test-suite.sh
+++ ubuntu/phoronix-test-suite.sh
@@ -2,7 +2,9 @@
 
 ## Install PTS and benchmark your gear:
 
-sudo apt install phoronix-test-suite
+echo "sudo apt install phoronix-test-suite"
+
+echo "http://www.phoronix-test-suite.com/?k=downloads"
 
 # Run the benchmark:
 
