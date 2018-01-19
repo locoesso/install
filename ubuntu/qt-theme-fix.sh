356ffb3653aca3bafc75b61fc698751bae135030
Esa Hanikka <esa.hanikka@gmail.com>
2018-01-19 11:22:36 +0200

    qt-theme-fix.sh fixed

--- ubuntu/qt-theme-fix.sh
+++ ubuntu/qt-theme-fix.sh
@@ -4,8 +4,6 @@ sudo add-apt-repository ppa:mati75/qt5ct
 sudo apt update
 sudo apt install qt4-qtconfig qt5ct
 
-echo "export QT_QPA_PLATFORMTHEME=qt5ct" >> ~/.profile
-echo "export QT_QPA_PLATFORMTHEME=qt5ct >> /etc/environment"
-sudo nano /etc/environment
-
+echo "export QT_QPA_PLATFORMTHEME=qt5ct" | tee -a ~/.profile
+sudo echo "export QT_QPA_PLATFORMTHEME=qt5ct" | sudo tee -a /etc/environment
 
