dfc3f265e5e2323413cf61aa605515264cd8bf4b
Esa Hanikka <esa.hanikka@gmail.com>
2018-02-01 13:20:04 +0200

    x

--- ubuntu/minecraft.sh
+++ ubuntu/minecraft.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo sudo apt install openjdk-9 openjdk-9-jre-headless
-sudo mkdir -p /opt/minecraft
-sudo wget http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar -O /opt/minecraft/Minecraft.jar
-sudo cp minecraft.desktop /usr/share/applications/
+# sudo sudo apt install openjdk-9 openjdk-9-jre-headless
+mkdir -p ~/.games/minecraft
+wget http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar -O ~/.games/minecraft/Minecraft.jar
+cp minecraft.desktop ~/.local/share/applications/
 
 
