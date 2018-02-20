264311dbf269654122c53b66119e51305b1cfaf0
Esa Hanikka <esa.hanikka@gmail.com>
2018-02-20 19:19:24 +0200

    x

--- ubuntu/virtualbox.sh
+++ ubuntu/virtualbox.sh
@@ -1,4 +1,4 @@
 echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" | sudo tee -a /etc/apt/sources.list
 sudo apt-key add oracle_vbox.asc
-sudo apt-get update
-sudo apt-get install virtualbox-4.3
+sudo apt update
+sudo apt search virtualbox
