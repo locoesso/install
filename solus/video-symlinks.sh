0d5174465c69c7d82a5352f02758e6a12ead0993
Esa Hanikka <esa.hanikka@gmail.com>
2018-03-19 17:06:23 +0200

    updated new partiotions

---
+++ solus/video-symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s /media/Altair/Video Altair
+ln -s /media/Betelgeuse/Video Betelgeuse
+ln -s /media/Capella/Video Capella
+ln -s /media/Deneb/Video Deneb
+
+ln -s /media/Altair/Video/TV TV
+
