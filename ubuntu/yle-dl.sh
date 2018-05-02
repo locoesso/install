245f39b27c58a676cbd3ed37d2570bae47400921
locoesso <esa.hanikka@gmail.com>
2018-05-02 11:23:05 +0300

    Blender nvidia cuda

--- ubuntu/yle-dl.sh
+++ ubuntu/yle-dl.sh
@@ -2,6 +2,6 @@
 
 # https://github.com/aajanki/yle-dl
 
-sudo apt install rtmpdump wget python python-setuptools python-pip python-crypto python-requests python-lxml php-cli php-curl php-mcrypt php-xml php-bcmath mcrypt
-sudo pip install yle-dl
+sudo apt install rtmpdump wget python python-setuptools python-pip python-crypto python-requests python-lxml php-cli php-curl php-xml php-bcmath mcrypt
+sudo -H pip install yle-dl
 
