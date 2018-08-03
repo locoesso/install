17e63efd186b3b453b66a8ec0c203179db5520fd
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-03 08:37:50 +0300

    ...

--- ubuntu/virtualbox.sh
+++ ubuntu/virtualbox.sh
@@ -1,4 +1,8 @@
-echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee -a /etc/apt/sources.list
+#!/bin/sh
+
+# echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee -a /etc/apt/sources.list
+
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt update
