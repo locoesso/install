78d3c457c242cd6573815a296ac463b477c820b3
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-09 11:44:06 +0300

    Cockpit

---
+++ fedora/cockpit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# https://cockpit-project.org/
+
+sudo dnf install cockpit
+sudo systemctl enable --now cockpit.socket
+sudo firewall-cmd --add-service=cockpit
+sudo firewall-cmd --add-service=cockpit --permanent
