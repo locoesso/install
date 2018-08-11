ffa25a2b278c514c6d3f31edd49886eea30168d1
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-11 09:39:14 +0300

    MEGAsync fix if using qt5ct

--- ubuntu/hardcode-tray.sh
+++ ubuntu/hardcode-tray.sh
@@ -6,3 +6,6 @@ sudo add-apt-repository ppa:andreas-angerer89/sni-qt-patched
 sudo apt update
 sudo apt install sni-qt hardcode-tray
 # sudo apt install sni-qt sni-qt:i386 hardcode-tray
+
+echo "MEGAsync fix if using qt5ct, start megasync with:"
+echo "QT_QPA_PLATFORMTHEME=''; megasync"
