0632a71e6e1d423778ba7b9fe42092bdc4eaecb1
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-11 08:58:07 +0300

    Added papirus-folders

--- ubuntu/papirus-icon-theme.sh
+++ ubuntu/papirus-icon-theme.sh
@@ -1,4 +1,3 @@
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt update
-sudo apt install papirus-icon-theme libreoffice-style-papirus
-
+sudo apt install papirus-icon-theme libreoffice-style-papirus papirus-folders
