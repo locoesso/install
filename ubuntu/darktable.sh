7583a485da944ccc26d6f511fe8911d36b66582b
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-14 13:14:29 +0300

    darktable

---
+++ ubuntu/darktable.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# https://launchpad.net/~pmjdebruijn/+archive/ubuntu/darktable-release
+
+sudo add-apt-repository ppa:pmjdebruijn/darktable-release
+sudo apt update
+sudo apt install darktable
