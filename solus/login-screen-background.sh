63e567590fb3e25ca4728bfb2c47dab14cb99623
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-25 13:31:04 +0300

    fixed path

--- solus/login-screen-background.sh
+++ solus/login-screen-background.sh
@@ -2,6 +2,6 @@
 
 sudo mkdir -p /etc/lightdm
 echo "[Greeter]" | sudo tee -a /etc/lightdm/slick-greeter.conf
-echo "background=/etc/backgrounds/solus/SolusFresh.png" | sudo tee -a /etc/lightdm/slick-greeter.conf
+echo "background=/usr/share/backgrounds/solus/SolusFresh.png" | sudo tee -a /etc/lightdm/slick-greeter.conf
 
 sudo nano /etc/lightdm/slick-greeter.conf
