bd79234840372d55a849e466931803dd47e966b8
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-25 13:17:32 +0300

    Tilix VTE

--- solus/zsh-oh_my_zsh.sh
+++ solus/zsh-oh_my_zsh.sh
@@ -1,3 +1,10 @@
 sudo ln -s /usr/share/defaults/etc/shells /etc/shells
 sudo eopkg it zsh git curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Using with Tilix: "
+echo "
+    if [[ $TILIX_ID ]]; then
+        source /usr/share/defaults/etc/profile.d/vte.sh
+    fi
+"
+ls /usr/share/defaults/etc/profile.d/vte.sh
