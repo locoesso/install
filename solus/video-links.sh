988c6009d96a8f5943fb30aacb4c73178cbb2632
Esa Hanikka <esa.hanikka@gmail.com>
2018-10-24 12:47:03 +0300

    Removed old video symlink

--- solus/video-links.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ln -s /media/Delta/Video/TV ~/Videos/TV
-ln -s /media/Delta/Video/ ~/Videos/Delta
-ln -s /media/Epsilon/Video/ ~/Videos/Epsilon
-ln -s /media/Hadraniel/Video/ ~/Videos/Hadraniel
-ln -s /media/Pluto/Video/ ~/Videos/Pluto
