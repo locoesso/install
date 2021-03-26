d51e9bd89e62c86045cbf931c9104172c0e7c3d2
Esa Hanikka <esa.hanikka@gmail.com>
2021-03-26 14:20:01 +0200

    x

--- rsync-backup/backup.sh
+++ rsync-backup/backup.sh
@@ -1,3 +1,3 @@
 # sudo rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","swapfile1","/home/esso/VirtualBox VMs/","/home/esso/Downloads"} /mnt/chara/rsync/Solus/
 
-sudo rsync -aAXv --delete --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude="swapfile*" --exclude="lost+found" --exclude=".cache" --exclude="Downloads" --exclude="VirtualBox VMs" --exclude=".ecryptfs" / /mnt/chara/rsync/Solus
+sudo rsync -aAXv --delete --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude="swapfile*" --exclude="lost+found" --exclude=".cache" --exclude="Downloads" --exclude="VirtualBox VMs" --exclude=".local/share/Steam/steamapps/common" --exclude=".ecryptfs" / /mnt/chara/rsync/Solus
