c844a57d1077c7856c6f774e3d70b03f7c87a9c1
Esa Hanikka <esa.hanikka@gmail.com>
2021-03-26 10:58:14 +0200

    x

--- solus/minecraft.sh
+++ solus/minecraft.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo eopkg it openjdk-8
+# sudo eopkg it openjdk-8
 sudo mkdir -p /opt/minecraft
 sudo wget http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar -O /opt/minecraft/Minecraft.jar
 sudo cp minecraft.desktop /usr/share/applications/
